91c977eee6b04e95b20c8007d9f1e71c5d96bec1
Emmanuel Akpan <mfonakpan@gmail.com>
2020-07-08 15:37:22 +0100

    Mini-Calculator App
    A simple HTML, CSS, and JavaScript mini calculator app

---
+++ app.js
@@ -0,0 +1,22 @@
+﻿function addFunc(){
+
+x = parseInt(document.getElementById("num1").value);
+y = parseInt(document.getElementById("num2").value);
+
+result = x+y
+document.getElementById("output").innerHTML = result
+
+}
+function minusFunc(){
+x = parseInt(document.getElementById("num1").value);
+y = parseInt(document.getElementById("num2").value);
+result = x-y
+document.getElementById("output").innerHTML = result
+
+}
+
+function myFunction() {
+	document.getElementById("frm1").reset();
+	
+}
+
